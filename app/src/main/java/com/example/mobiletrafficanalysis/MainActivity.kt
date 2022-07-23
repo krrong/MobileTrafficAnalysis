c7fcdd1596d8cbd41ef192c41d6ced7201e4fe5b
krrong <khk9664@naver.com>
2022-07-24 00:10:09 +0900

    런타임 퍼미션 체크 기능 추가
    런타임 퍼미션 체크 기능 추가 but 정상동작 X
    
    TODO : 퍼미션을 주었음에도 true를 반환하지 않고 지속적으로 퍼미션을 요청함 해당 부분 수정 필요

--- app/src/main/java/com/example/mobiletrafficanalysis/MainActivity.kt
+++ app/src/main/java/com/example/mobiletrafficanalysis/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.mobiletrafficanalysis
 
-import android.app.usage.NetworkStats
-import android.app.usage.NetworkStatsManager
+import android.app.AppOpsManager
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import android.os.RemoteException
 import android.provider.Settings
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -19,6 +19,7 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
     val appList = ArrayList<String>()
     val byteList = ArrayList<Long>()
+    private val REQUEST_CODE = 1004
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,61 +36,17 @@ class MainActivity : AppCompatActivity() {
         val packageManager = packageManager
         val list = packageManager.getInstalledApplications(0)
 
-        for (i in list.indices){
+        for (i in list.indices) {
 //            appList.add(list[i].packageName)
 //            val receiveByteByMobile = TrafficStats.getUidRxBytes(list[i].uid);
 //            byteList.add(receiveByteByMobile)
         }
 
-        try {
-            val networkStatsManager =
-                applicationContext.getSystemService(Context.NETWORK_STATS_SERVICE) as NetworkStatsManager
-
-            val networkStats = networkStatsManager.queryDetailsForUid(
-                NetworkCapabilities.TRANSPORT_WIFI,
-                "",
-                0,
-                System.currentTimeMillis(),
-                0
-            )
-
-            do {
-                val bucket = NetworkStats.Bucket()
-                networkStats.getNextBucket(bucket)
-
-                val txByte = bucket.txBytes
-                byteList.add(txByte)
-
-            } while (networkStats.hasNextBucket())
-        }
-        catch (e : Exception){
-            e.printStackTrace()
-            val builder = AlertDialog.Builder(this)
-            builder.setMessage("허용해줭")
-            builder.setPositiveButton(
-                "확인",
-                DialogInterface.OnClickListener{dialog, id ->
-                    val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
-                    this.startActivity(intent)
-                }
-            )
-            builder.setNegativeButton(
-                "취소",
-                DialogInterface.OnClickListener{dialog, id ->
-
-                }
-            )
-
-            val alertDialog = builder.create()
-            alertDialog.show()
-        }
-
         initView()
-
+        checkPermission()
     }
 
-
-    private fun initView(){
+    private fun initView() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val appAdapter = AppAdapter(this, appList)
         val layoutManager = LinearLayoutManager(this)
@@ -97,5 +54,69 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = appAdapter
         recyclerView.layoutManager = layoutManager
         recyclerView.setHasFixedSize(true)
+    }
+
+    /**
+     * 런타임 퍼미션 체크 (PACKAGE_USAGE_STATS)
+     */
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this, android.Manifest.permission.PACKAGE_USAGE_STATS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.e("TEST", "PERMISSION_GRANTED")
+        } else {
+            showDialog()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("TEST", "GRANTED")
+                } else {
+                    showDialog()
+                }
+            }
+        }
+    }
+
+    /**
+     * 사용자가 직접 사용정보 접근 허용할 수 있도록 다이얼로그 생성
+     */
+    private fun showDialog() {
+        // 다이얼로그 빌더 생성
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage(
+            """
+            앱을 사용하기 위해서는 앱 실행 기록을 제공해야 합니다.
+            설정을 수정하시겠습니까?
+            """.trimIndent()
+        )
+        builder.setCancelable(true)
+
+        // 다이얼로그에 "설정" 버튼 추가 및 리스너 바인딩
+        builder.setPositiveButton("설정") { dialog, id -> // 설정으로 들어가 바로 수정할 수 있도록 인텐트 실행
+            val settingIntent = Intent(
+                Settings.ACTION_USAGE_ACCESS_SETTINGS,
+            )
+            startActivity(settingIntent)
+        }
+
+        // 다이얼로그에 "취소" 버튼 추가 및 리스너 바인딩
+        builder.setNegativeButton("취소"){dialog, id ->
+            // 무시
+        }
+
+        val alertDialog = builder.create()
+        alertDialog.show()
     }
 }
