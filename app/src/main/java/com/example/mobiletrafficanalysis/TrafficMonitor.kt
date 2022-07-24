5f19f562ebed287f348f845e78387822355f010b
krrong <khk9664@naver.com>
2022-07-25 00:49:37 +0900

    TrafficMonitor 클래스 내용 추가
    TrafficMonitor 클래스 내용 추가
    
    1. 스레드를 상속 받아 스레드로 사용할 수 있도록 변경
    
    2. 각 어플리케이션의 uid를 이용하여 총 송신 트래픽을 받아올 수 있도록 수정
    
    workflow :
    packageManager 를 통해 휴대폰에 설치된 모든 앱의 패키지명(앱 이름)과 uid를 가져옴
    
    networkStatsManager와 Bucket을 통해 uid를 이용하여 총 송신 트래픽을 계산
    
    어댑터의 데이터인 appDataList에 패키지명과 송신 트래픽 양을 추가

--- app/src/main/java/com/example/mobiletrafficanalysis/TrafficMonitor.kt
+++ app/src/main/java/com/example/mobiletrafficanalysis/TrafficMonitor.kt
@@ -1,7 +1,46 @@
 package com.example.mobiletrafficanalysis
 
-import android.net.TrafficStats
+import android.app.usage.NetworkStats
+import android.app.usage.NetworkStatsManager
+import android.content.Context
+import android.net.NetworkCapabilities
+import android.util.Log
 
-class TrafficMonitor : TrafficStats() {
-    
+
+class TrafficMonitor(private var context: Context, private var appDataList : ArrayList<Data>) : Thread() {
+    override fun run() {
+        // 어플리케이션의 패키지명 가져오기
+        val packageManager = context.packageManager
+        val list = packageManager.getInstalledApplications(0)
+        Log.d("TrafficMonitor", list.size.toString())
+
+        // networkStatsManager 생성
+        val networkStatsManager =
+            context.getSystemService(Context.NETWORK_STATS_SERVICE) as NetworkStatsManager
+
+        for (app in list) {
+            // 각 앱의 uid
+            val uid = app.uid
+
+            // uid 를 통해 NetworkStats 리턴
+            val networkStats = networkStatsManager.queryDetailsForUid(
+                NetworkCapabilities.TRANSPORT_WIFI,
+                "",
+                0,
+                System.currentTimeMillis(),
+                uid
+            )
+
+            // uid 를 가진 앱의 총 송신 트래픽 계산
+            var txBytes: Long = 0
+            do {
+                val bucket = NetworkStats.Bucket()
+                networkStats.getNextBucket(bucket)
+                txBytes += bucket.txBytes
+            } while (networkStats.hasNextBucket())
+
+            // appDataList 에 (패키지명, 총 송신 트래픽) 추가
+            appDataList.add(Data(app.packageName, txBytes))
+        }
+    }
 }
