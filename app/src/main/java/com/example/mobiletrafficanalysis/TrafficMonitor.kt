92e8279581876e9b3efd992f3abeabec1b22fe2d
krrong <khk9664@naver.com>
2022-07-27 00:25:58 +0900

    스레드 재실행 시 중복 데이터가 들어가지 않도록 수정
    3초마다 주기적으로 TrafficMonitor()의 getTxBytesForAllApp()함수를 재실행 함
    
    리사이클러뷰 어댑터는 패키지명과 촉 송신 트래픽 양을 저장
    
    함수가 재실행 될 때마다 데이터 개수가 늘어나는 문제 발생
    
    hashmap을 사용하여 중복 검사 후 넣는 방식으로 해결
    
    TODO : 더 좋은 해결 방법이 있다면 고려해봐야 함.
    정확하지는 않지만 함수 수행 시간이 7초로 계산됨

--- app/src/main/java/com/example/mobiletrafficanalysis/TrafficMonitor.kt
+++ app/src/main/java/com/example/mobiletrafficanalysis/TrafficMonitor.kt
@@ -7,17 +7,24 @@ import android.net.NetworkCapabilities
 import android.util.Log
 
 
-class TrafficMonitor(private var context: Context, private var appDataList : ArrayList<Data>, private var whiteList : ArrayList<String>) : Thread() {
-    override fun run() {
-        // 어플리케이션의 패키지명 가져오기
+class TrafficMonitor(private var context: Context, private var appDataList : ArrayList<Data>, private var whiteList : ArrayList<String>){
+
+    fun getTxBytesForAllApp() {
+        // 설치되어 있는 어플리케이션의 패키지명 가져오기
         val packageManager = context.packageManager
         val list = packageManager.getInstalledApplications(0)
-        Log.d("TrafficMonitor", list.size.toString())
 
         // networkStatsManager 생성
         val networkStatsManager =
             context.getSystemService(Context.NETWORK_STATS_SERVICE) as NetworkStatsManager
 
+        // 어댑터에 이미 들어있는 어플리케이션의 데이터를 따로 저장 --> 어댑터에 중복으로 들어가지 않게 하기 위함
+        val packageNameList : HashMap<String, Data> = HashMap()
+        for (app in appDataList){
+            packageNameList.put(app.getPackageName(), app)
+        }
+
+        // 설치되어 있는 앱 별 총 송신 트래픽 계산
         for (app in list) {
             // 각 앱의 uid
             val uid = app.uid
@@ -30,8 +37,7 @@ class TrafficMonitor(private var context: Context, private var appDataList : Arr
                 System.currentTimeMillis(),
                 uid
             )
-
-            // uid 를 가진 앱의 총 송신 트래픽 계산
+            // uid 인 앱의 총 송신 트래픽 계산
             var txBytes: Long = 0
             do {
                 val bucket = NetworkStats.Bucket()
@@ -39,12 +45,27 @@ class TrafficMonitor(private var context: Context, private var appDataList : Arr
                 txBytes += bucket.txBytes
             } while (networkStats.hasNextBucket())
 
-            // appDataList 에 (패키지명, 총 송신 트래픽) 추가
-            appDataList.add(Data(app.packageName, txBytes))
+            // 원래 어댑터에 없던 것이라면 추가
+            if(packageNameList.get(app.packageName) == null){
+                // appDataList 에 (패키지명, 총 송신 트래픽) 추가
+                appDataList.add(Data(app.packageName, txBytes))
+            }
+            // 원래 어댑터에 있던 것이라면 txBytes 만 수정
+            else{
+                val data = packageNameList.get(app.packageName)
+                val index = appDataList.indexOf(data)
+                appDataList[index].setTxBytes(txBytes)
+            }
+
+            networkStats.close()
 
             // com.google.* or com.samsung.* 으로 시작하는 패키지명은  whiteList 에 등록
             if(app.packageName.startsWith("com.google.") || app.packageName.startsWith("com.samsung.")){
-                whiteList.add(app.packageName)
+//                whiteList.add(app.packageName)
+                // 이미 화이트리스트에 등록되어 있지 않다면 추가
+                if(!whiteList.contains(app.packageName)){
+                    whiteList.add(app.packageName)
+                }
             }
         }
     }
