86d8632a6c3f25a910cba9e3b2b7a0229251860c
krrong <khk9664@naver.com>
2022-07-28 20:48:57 +0900

    멤버 변수에 uid추가
    멤버 변수에 uid추가

--- app/src/main/java/com/example/mobiletrafficanalysis/Data.kt
+++ app/src/main/java/com/example/mobiletrafficanalysis/Data.kt
@@ -2,10 +2,12 @@ package com.example.mobiletrafficanalysis
 
 public class Data {
     private var packageName : String = ""
+    private var uid : Int = 0
     private var txBytes : Long = 0
 
-    constructor(packageName:String, txBytes:Long){
+    constructor(packageName:String, uid : Int, txBytes:Long){
         this.packageName = packageName
+        this.uid = uid
         this.txBytes = txBytes
     }
 
@@ -17,11 +19,19 @@ public class Data {
         return this.txBytes
     }
 
+    fun getUid(): Int {
+        return this.uid
+    }
+
     fun setPackageName(packageName:String) {
         this.packageName = packageName
     }
 
     fun setTxBytes(txBytes:Long) {
         this.txBytes = txBytes
+    }
+
+    fun setUid(uid:Int) {
+        this.uid = uid
     }
 }
