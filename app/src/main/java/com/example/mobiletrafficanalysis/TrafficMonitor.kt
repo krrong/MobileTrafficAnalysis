861c594ecc9a4449c46d98b10cf68d3b9c4c0e15
krrong <khk9664@naver.com>
2022-07-31 23:57:16 +0900

    모듈화, diff 계산 로직 변경
    1. 모듈화
    패키지명이 화이트 리스트에 있는지 반환하는 함수, 현재 시간을 일정 포맷으로 변경하여 반환해주는 함수로 모듈화 진행
    
    2. diff 계산 로직 변경
    동일한 uid를 가진 버킷이 있을 수 있기 때문에 모든 버킷에 대해 uid별 송신 트래픽을 계산하고, 이후에 history와 비교하여 diff를 계산하도록 수정

--- app/src/main/java/com/example/mobiletrafficanalysis/TrafficMonitor.kt
+++ app/src/main/java/com/example/mobiletrafficanalysis/TrafficMonitor.kt
@@ -18,30 +18,49 @@ class TrafficMonitor(private var context: Context,
 
     private val period : Int = 10000
 
-    fun getTxBytesForAllApp() {
-//        // 함수 시작 시간 Log
-//        val dataFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-//        val time = System.currentTimeMillis()
-//        Log.e("getTxBytesForAllApp", dataFormat.format(time))
+    /**
+     * 패키지 명이 화이트 리스트에 있는지 반환
+     */
+    fun isInWhiteList(packageName : String) : Boolean {
+        return whiteList.contains(packageName)
+    }
 
+    /**
+     * 현재 시간을 yyyy-MM-dd HH:mm:ss 포맷으로 반환
+     */
+    fun getTime() : String{
+        val dataFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val time = System.currentTimeMillis()
+        return dataFormat.format(time)
+    }
+
+    /**
+     * 송신 트래픽이 발생한 어플리케이션의 송신 트래픽 계산
+     */
+    fun getTxBytesForAllApp() {
         // networkStatsManager 생성
         val networkStatsManager =
             context.getSystemService(Context.NETWORK_STATS_SERVICE) as NetworkStatsManager
 
-        // 어댑터에 이미 들어있는 어플리케이션의 데이터를 따로 저장 --> 어댑터에 중복으로 들어가지 않게 하기 위함
+        // packageNameList 는 uid를 이용하여 Data 객체를 가져오는 용도로 사용
+        // measureTxBytes 는 같은 uid에서 발생한 txBytes를 계산하고 history와 비교하는 용도로 사용
         val packageNameList : HashMap<Int, Data> = HashMap()
+        var measureTxBytes : HashMap<Int, Data> = HashMap()
+
         for (app in list){
             // 앱 이름 얻어오기
             val label = context.packageManager.getApplicationLabel(app)
 
-            // 히스토리에 없다면 송신 데이터 양은 0으로 세팅하여 추가
-            if(appHistory.get(app.uid) == null){
-                packageNameList.put(app.uid, Data(label.toString() + "(" + app.packageName + ")", "", app.uid, 0L))
-            }
-            // 히스토리에 있다면 송신 데이터 양은 히스토리에서 얻어와서 추가
-            else{
-                packageNameList.put(app.uid, Data(label.toString() + "(" + app.packageName + ")", "", app.uid, appHistory.get(app.uid)!!))
-            }
+//            // 히스토리에 없다면 송신 데이터 양은 0으로 세팅하여 추가
+//            if(appHistory.get(app.uid) == null){
+//                packageNameList.put(app.uid, Data(label.toString() + "(" + app.packageName + ")", "", app.uid, 0L))
+//            }
+//            // 히스토리에 있다면 송신 데이터 양은 히스토리에서 얻어와서 추가
+//            else{
+//                packageNameList.put(app.uid, Data(label.toString() + "(" + app.packageName + ")", "", app.uid, appHistory.get(app.uid)!!))
+//            }
+            // packageNameList 는 uid를 이용하여 Data 객체를 가져오는 용도로 사용
+            packageNameList.put(app.uid, Data(label.toString() + "(" + app.packageName + ")", "", app.uid, 0L))
         }
 
         // NetworkStats 리턴
@@ -53,50 +72,102 @@ class TrafficMonitor(private var context: Context,
         )
 
         do {
+            // 같은 uid를 가진 버킷이 여러개 있을 수 있음
+            //  --> 버킷을 돌면서 같은 uid에서 나온 txbytes는 모두 더해서 history와 비교해야함
+
             //앱의 총 송신 트래픽 계산
             val bucket = NetworkStats.Bucket()
             networkStats.getNextBucket(bucket)
 
             val uid = bucket.uid    // 앱 uid 반환
 
-            val data = packageNameList.get(uid) // uid를 사용하여 (앱이름 + 패키지명, uid, 송신 트래픽 양) 반환
-
-            // 기본 시스템 앱은 pass
+            // 기본 시스템 앱 pass
             if(uid == 1000 || uid == 0) continue
 
-            // 데이터가 null 이 아니고 화이트리스트에 들어가 있지 않다면 송신 트래픽 측정
-            if(data != null && !whiteList.contains(data.getPackageName())) {
-                Log.e("UID", uid.toString())
-                Log.e("txBytes", bucket.txBytes.toString())
+            val data = packageNameList.get(uid) // uid를 사용하여 (앱이름 + 패키지명, uid, 송신 트래픽 양) 반환
 
-                var txBytes = bucket.txBytes   // 앱 별 송신 트래픽
-                val bytes = data?.getTxBytes()
-                val diff = txBytes - bytes!!    // 현재 계산한 txBytes - 이전에 계산한 txBytes
+            if(data != null){
+                if(!isInWhiteList(data.getPackageName())){
+                    Log.e("UID", uid.toString())
+                    Log.e("txBytes", bucket.txBytes.toString())
 
-                // 차이가 양수면 송신 기록이 있다는 의미-> 사용자에게 보여줘야 함
-                // 음수면 송신 데이터 양이 적어졌다는 뜻 --> 측정 시간동안 보내지 않았다고 할 수 있음
-                if (diff > 0L) {
-//                if (diff != 0L) {
-                    appHistory.put(uid, diff)
-//                    if(appHistory.get(uid) == null){
-//                        appHistory.put(uid, diff)
-//                    }
-//                    else{
-//                        appHistory.replace(uid, diff)
-//                    }
-                    val dataFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-                    val time = System.currentTimeMillis()
-                    data.setTxBytes(diff)
-                    data.setDetectTime(dataFormat.format(time))
+                    val curTxBytes = bucket.txBytes
 
-                    // 가장 앞에 새로운 데이터 추가
-                    appDataList.add(0, data)
+                    // measureTxBytes 에 uid가 없다면 처음 발생한 송신 트래픽
+                    //  --> measureTxBytes 에 새로 추가해줘야 함
+                    if(measureTxBytes.contains(uid) == false){
+                        data.setTxBytes(curTxBytes)
+                        measureTxBytes.put(uid, data)
+                    }
+                    // measureTxBytes 가 uid가 있다면 이전에 해당 uid에서 송신 트래픽이 발생한 적 있음
+                    //  --> 이전 송신 트래픽과 더해줘야 함
+                    else{
+                        // 이전 송신 트래픽 값
+                        val prevTxBytes = measureTxBytes.get(uid)?.getTxBytes()
+
+                        if (prevTxBytes != null) {
+                            measureTxBytes.get(uid)?.setTxBytes(prevTxBytes + curTxBytes)
+                        }
+                    }
                 }
             }
+
+//            // data 가 유효하다면
+//            if(data != null) {
+//                // 화이트리스트에 들어가있지 않다면 송신 트래픽 측정
+//                if(!isInWhiteList(data.getPackageName())) {
+//                    Log.e("UID", uid.toString())
+//                    Log.e("txBytes", bucket.txBytes.toString())
+//
+//                    var txBytes = bucket.txBytes    // 앱 별 송신 트래픽
+//                    val bytes = data?.getTxBytes()
+//                    val diff = txBytes - bytes!!    // 현재 계산한 txBytes - 이전에 계산한 txBytes
+//
+//                    // 차이가 양수면 송신 기록이 있다는 의미-> 사용자에게 보여줘야 함
+//                    // 음수면 송신 데이터 양이 적어졌다는 뜻 --> 측정 시간동안 보내지 않았다고 할 수 있음
+//                    if (diff > 0L) {
+////                if (diff != 0L) {
+//                        appHistory.put(uid, diff)
+////                    if(appHistory.get(uid) == null){
+////                        appHistory.put(uid, diff)
+////                    }
+////                    else{
+////                        appHistory.replace(uid, diff)
+////                    }
+//                        // data 값 변경
+//                        data.setTxBytes(diff)
+//                        data.setDetectTime(getTime())
+//
+//                        // 가장 앞에 새로운 데이터 추가
+//                        appDataList.add(0, data)
+//                    }
+//                }
+//            }
         } while (networkStats.hasNextBucket())
-
-
-
         networkStats.close()
+
+        // appHistory와 버킷에서 계산한 값들의 차이를 확인
+        for(iter in measureTxBytes){
+            var data = iter.value
+            
+            // appHistory에 없다면 appHistory와 appDataList에 추가
+            if(appHistory.contains(data.getUid()) == false){
+                appHistory.put(data.getUid(), data.getTxBytes())
+                data.setDetectTime(getTime())
+                appDataList.add(data)
+            }
+            // appHistory에 있다면 차이 계산
+            else{
+                val diff = data.getTxBytes() - appHistory.get(data.getUid())!!
+                
+                // 차이가 0보다 크면 송신 트래픽이 있다는 의미
+                //  --> appDataList에 추가
+                if(diff > 0){
+                    data.setDetectTime(getTime())
+                    appDataList.add(0, data)
+                    appHistory.put(data.getUid(), diff)
+                }
+            }
+        }
     }
 }
