aca09370ac48aeb5c02d2c9cc5992bfb01cb8d38
krrong <khk9664@naver.com>
2022-09-03 19:41:28 +0900

    생성자로 화이트리스트 매니저 추가, addImageView 클릭 리스너 추가
    1. 생성자로 화이트리스트 매니저 추가
    -> 화이트리스트 매니저를 생성자 인자로 넘겨줌으로써 앱에서 같은 화이트리스트를 사용할 수 있도록 함
    
    2. addImageView 클릭 리스너 추가
    -> 선택한 앱이 화이트리스트에 있다면 addImageView 터치 시 화이트리스트에서 삭제 및 이미지 변경, 선택한 앱이 화이트리스트에 없는 경우도 동일

--- app/src/main/java/com/example/mobiletrafficanalysis/Adapter/WhiteListAdapter.kt
+++ app/src/main/java/com/example/mobiletrafficanalysis/Adapter/WhiteListAdapter.kt
@@ -1,18 +1,20 @@
 package com.example.mobiletrafficanalysis.Adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mobiletrafficanalysis.Activity.MainActivity
 import com.example.mobiletrafficanalysis.Class.AppInfo
+import com.example.mobiletrafficanalysis.Class.whiteListManager
 import com.example.mobiletrafficanalysis.R
 
-class WhiteListAdapter(val dataList : ArrayList<AppInfo>) : RecyclerView.Adapter<WhiteListAdapter.ViewHolder>()  {
+class WhiteListAdapter(val dataList : ArrayList<AppInfo>, val whiteListManager: whiteListManager) : RecyclerView.Adapter<WhiteListAdapter.ViewHolder>()  {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.app_whitelist, parent, false)
-
         return ViewHolder(view)
     }
 
@@ -33,8 +35,10 @@ class WhiteListAdapter(val dataList : ArrayList<AppInfo>) : RecyclerView.Adapter
             appName?.text = item.getAppName()
             imageView.setImageDrawable(item.getIcon())
             isInWhiteList(item, addImageView)
+            setAddImageViewClickListener(item.getUid())
         }
 
+        // 화이트리스트에 있는지 반환
         fun isInWhiteList(item : AppInfo, addImageView: ImageView){
             if(item.getinWhiteList() == true){
                 addImageView.setImageResource(R.drawable.minus)
@@ -42,6 +46,24 @@ class WhiteListAdapter(val dataList : ArrayList<AppInfo>) : RecyclerView.Adapter
             else{
                 addImageView.setImageResource(R.drawable.plus)
             }
+        }
+
+        // 
+        fun setAddImageViewClickListener(uid : Int){
+            addImageView.setOnClickListener(View.OnClickListener {
+                // 화이트리스트에 있을 때 터치 시 화이트리스트에서 삭제 및 minus 이미지로 변경
+                if(whiteListManager.isinWhiteList(uid) == true){
+                    addImageView.setImageResource(R.drawable.plus)
+                    whiteListManager.delete(uid)
+                    Log.e("WhiteListAdapter", "addImageView plus -> minus")
+                }
+                // 화이트리스트에 없을 때 터치 시 화이트리스트에 추가 및 plus 이미지로 변경
+                else{
+                    addImageView.setImageResource(R.drawable.minus)
+                    whiteListManager.add(uid)
+                    Log.e("WhiteListAdapter", "addImageView minus -> plus")
+                }
+            })
         }
     }
 }
