c1d8ddc6ae5ecb26f8f92559d7724bfbb14a28f9
krrong <khk9664@naver.com>
2022-09-03 19:39:23 +0900

    화이트리스트 아이템 클래스에 uid를 함께 저장
    uid를 통해 앱이 화이트리스트에 있는지 쉽게 확인하기 위함
    
    TODO : 화이트리스트에 존재하는지 여부를 저장하는 플래그가 필요한지 고민 필요, 필요없다면 삭제

--- app/src/main/java/com/example/mobiletrafficanalysis/Class/AppInfo.kt
+++ app/src/main/java/com/example/mobiletrafficanalysis/Class/AppInfo.kt
@@ -7,10 +7,13 @@ class AppInfo {
     private var appName : String = ""
     private var icon : Drawable
     private var inWhiteList : Boolean = false   // 화이트 리스트에 있는지 여부
+    private var uid : Int = 0   // 앱의 uid
 
-    constructor(appName:String, image:Drawable){
+    constructor(appName:String, image:Drawable, inWhiteList : Boolean, uid:Int){
         this.appName = appName
         this.icon = image
+        this.inWhiteList = inWhiteList
+        this.uid = uid
     }
 
     fun getAppName() : String{
@@ -25,6 +28,10 @@ class AppInfo {
         return this.inWhiteList
     }
 
+    fun getUid() : Int{
+        return this.uid
+    }
+
     fun setAppName(appName: String){
         this.appName = appName
     }
@@ -35,5 +42,9 @@ class AppInfo {
 
     fun setinWhiteList(flag : Boolean){
         this.inWhiteList = flag
+    }
+
+    fun setUid(uid : Int){
+        this.uid = uid
     }
 }
