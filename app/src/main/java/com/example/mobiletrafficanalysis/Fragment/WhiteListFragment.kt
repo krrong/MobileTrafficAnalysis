fdb154cf223ddc533d221b069f86c5c89539ef89
krrong <khk9664@naver.com>
2022-09-05 22:28:57 +0900

    화이트리스트매니저 추가, SharedPreference에서 화이트리스트 가져옴, 화이트리스트 저장버튼 추가, inWhiteList() 함수 추가
    1. 화이트리스트 매니저 추가
    -> 전체적으로 사용하는 화이트리스트를 관리하는 화이트리스트 매니저 추가
    
    2. SharedPreference에 저장된 화이트리스트 가져와서 세팅
    -> 이전에 저장해둔 SharedPreference에서 화이트리스트 로드해옴
    
    3. 화이트리스트 저장버튼 추가
    -> SAVE버튼을 통해 현재 화이트리스트 매니저가 가지고 있는 화이트리스트를 SharedPreference에 저장 및 전역으로 사용하는 화이트리스트에도 저장
    
    4. inWhiteList() 함수 추가
    -> inWhiteList() 함수를 통해 uid로 화이트리스트에 존재하는지 확인

--- app/src/main/java/com/example/mobiletrafficanalysis/Fragment/WhiteListFragment.kt
+++ app/src/main/java/com/example/mobiletrafficanalysis/Fragment/WhiteListFragment.kt
@@ -1,5 +1,7 @@
 package com.example.mobiletrafficanalysis.Fragment
 
+import android.app.Activity
+import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.os.Bundle
 import android.util.Log
@@ -7,19 +9,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.mobiletrafficanalysis.Adapter.AppAdapter
+import com.example.mobiletrafficanalysis.Activity.MainActivity
 import com.example.mobiletrafficanalysis.Adapter.WhiteListAdapter
 import com.example.mobiletrafficanalysis.Class.AppInfo
-import com.example.mobiletrafficanalysis.Class.Data
+import com.example.mobiletrafficanalysis.Class.whiteListManager
 import com.example.mobiletrafficanalysis.R
 
 
 class WhiteListFragment : Fragment() {
-    var list = mutableListOf<ApplicationInfo>() // 설치된 어플리케이션의 정보를 저장하는 리스트
-    var dataList = ArrayList<AppInfo>()         // 어댑터에 추가할 앱 리스트 (패키지명, 총 송신 트래픽)으로 구성
+    var list = mutableListOf<ApplicationInfo>()         // 설치된 어플리케이션의 정보를 저장하는 리스트
+    var dataList = ArrayList<AppInfo>()                 // 어댑터에 추가할 앱 리스트 (패키지명, 총 송신 트래픽)으로 구성
+    private var whiteList = ArrayList<Int>()            // 화이트 리스트(MainActivity 에서 생성한 것으로 사용)
+    var whitelistManager : whiteListManager? = null     // 화이트리스트 매니저
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,24 +37,50 @@ class WhiteListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        makeWhiteList()
+        val mainActivity : Activity = MainActivity.getActivity()         // MainActivity 에서 받아와 사용
+
+        MainActivity.prefs = mainActivity.getSharedPreferences("packageNameSet", Context.MODE_PRIVATE)
+        whitelistManager = whiteListManager(mainActivity, dataList)
+        whitelistManager!!.initializeWhiteList()
+
+        whiteList = whitelistManager!!.loadWhiteList() as ArrayList<Int>
+
+//        getApplication()
         initView(view)
     }
 
     private fun initView(view: View){
-        var whiteListRecyclerView = view.findViewById<RecyclerView>(R.id.WhiteListRecyclerView) // 화이트 리스트 레이아웃의 리사이클러뷰
-        var WhiteListAdapter = WhiteListAdapter(dataList)  // 어댑터
-        var layoutManager = LinearLayoutManager(activity)
+        val whiteListRecyclerView = view.findViewById<RecyclerView>(R.id.WhiteListRecyclerView) // 화이트 리스트 레이아웃의 리사이클러뷰
+        val whiteListAdapter = WhiteListAdapter(dataList, whitelistManager!!)  // 어댑터
+        val layoutManager = LinearLayoutManager(activity)
         val dividerItemDecoration = DividerItemDecoration(whiteListRecyclerView?.context, layoutManager.orientation) // 구분선
 
-        whiteListRecyclerView?.adapter = WhiteListAdapter
+        whiteListRecyclerView?.adapter = whiteListAdapter
         whiteListRecyclerView?.layoutManager = layoutManager
         whiteListRecyclerView?.addItemDecoration(dividerItemDecoration)
         whiteListRecyclerView?.setHasFixedSize(true)
+
+        // 저장 버튼은 화이트리스트를 sharedPreferences로 저장 및 MainActivity의 whiteList에 저장
+        val saveBtn = view.findViewById<Button>(R.id.saveBtn)
+        saveBtn.setOnClickListener(View.OnClickListener {
+            whitelistManager!!.saveWhiteList()
+            MainActivity.setWhiteList(whitelistManager!!.getCurrentWhiteList())
+        })
+
+        // 테스트 버튼
+        // TODO : button2는 추후에 삭제 필요
+        val button2 = view.findViewById<Button>(R.id.button2)
+        button2.setOnClickListener(View.OnClickListener {
+            val testData = whitelistManager!!.loadWhiteList()
+            Log.e("TEST","ASDF")
+        })
     }
 
-    // TODO : 앱 목록을 리사이클러뷰에 보여주고 화이트 리스트에 등록할 수 있도록 기능 추가
-    private fun makeWhiteList(){
+
+    /**
+     * 설치된 어플리케이션을 리사이클러뷰 어댑터에 추가
+     */
+    private fun getApplication(){
         // 설치되어 있는 모든 어플리케이션의 패키지명 가져오기
         list = activity?.packageManager?.getInstalledApplications(0) as MutableList<ApplicationInfo>
 
@@ -57,7 +88,21 @@ class WhiteListFragment : Fragment() {
             val packageName = app.packageName
             val label = activity?.packageManager?.getApplicationLabel(app) as String
             val icon = activity?.packageManager?.getApplicationIcon(packageName)
-            dataList.add(AppInfo(label, icon!!))
+            val uid = app.uid
+
+            val isinWhiteList = inWhiteList(uid)
+            dataList.add(AppInfo(label, icon!!, isinWhiteList, uid))
         }
+    }
+
+    /**
+     * uid가 화이트리스트에 있는지 반환
+     */
+    fun inWhiteList(uid : Int) : Boolean {
+        // 화이트리스트에 패키지명이 포함되어 있으면 true 반환
+        if(whiteList.contains(uid)){
+            return true
+        }
+        return false
     }
 }
